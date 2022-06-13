a53fe6cc0eb8183847c52dd7f74fe999bb40186b
cosmofrens <cosmofriends.io@gmail.com>
2022-06-13 15:24:44 -0700

    Removing the links on channels

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ client.on('message', message =>{
 if (command === 'test'){
     message.channel.send('The bot is working')
 }
+
+if (message.content.includes("https://")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("No links here, " + message.author)
+  }
+
+
 })
 
 client.login(process.env.BOT_TOKEN)
