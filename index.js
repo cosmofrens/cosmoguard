fb522d4fb379beb79957b6093bc7b0efd6fe1f06
cosmofrens <cosmofriends.io@gmail.com>
2022-06-12 22:17:39 -0700

    Initial commit
    Yo!

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+
+const prefix = '!';
+
+const client= new Discord.Client({
+    allowedMentions: {
+        parse: ['users', 'roles'],
+        repliedUser: true,
+    },
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_PRESENCES",
+        "GUILD_MEMBERS",
+        "GUILD_MESSAGE_REACTIONS"
+    ],
+});
+
+client.on("ready", () => {
+console.log("Bot is online!")
+
+});
+
+client.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+//test command
+
+if (command === 'test'){
+    message.channel.send('The bot is working')
+}
+})
+
+client.login(process.env.BOT_TOKEN)
